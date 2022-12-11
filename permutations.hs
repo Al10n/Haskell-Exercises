be50e5f0aa8853f2195a3684e57ad61e12128509
Al10n <114783418+Al10n@users.noreply.github.com>
2022-12-11 17:42:24 +0100

    Delete permutations.hs

--- permutations.hs
+++
@@ -1,6 +0,0 @@
-permutations :: Eq a => [a] -> [[a]]
-permutations [] = [[]]
-permutations as = do a <- as
-                     let l = delete a as
-                     ls <- permutations l
-                     return $ a : ls
